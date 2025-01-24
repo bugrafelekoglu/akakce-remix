d7d7cfb34593ded1cab9b7e1e34905050babfc19
Bugra <bugrafelekoglu@gmail.com>
2025-01-24 16:43:51 +0300

    style(storageOptions): update class names for dark mode compatibility and improve layout in product route

--- app/components/custom/storageOptions.tsx
+++ app/components/custom/storageOptions.tsx
@@ -29,7 +29,7 @@ export const StorageOptions: FC<TStorageOptions> = ({
           key={option}
           value={option}
           aria-label={option}
-          className="bg-gray-100 border border-gray-300 rounded-md data-[state=on]:pointer-events-none data-[state=on]:bg-primary data-[state=on]:text-secondary"
+          className="bg-background border border-gray-300 dark:border-gray-700 rounded-md data-[state=on]:pointer-events-none data-[state=on]:bg-primary data-[state=on]:text-secondary"
         >
           {option}
         </ToggleGroupItem>
